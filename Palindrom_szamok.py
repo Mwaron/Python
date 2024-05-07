401e8d2a082192aa14a745783b6cc3dca74bd220
Mwaron <marton.aron08@gmail.com>
2024-05-07 12:57:44 +0200

    year 2015 day 6 -> starting (Calendar)

--- Palindrom_szamok.py
+++ Palindrom_szamok.py
@@ -1,19 +1,50 @@
 #Számokat fog bekérni és kiadni hogy az plaindrom-e vagy nem
 
-szam1 = str(input("Írj be egy számot: "))
-szam2 = str(input("Írj be egy számot: "))
+szam1 = input("Írj be egy számot: ")
+szam2 = input("Írj be egy számot: ")
+
+if szam2 < szam1:
+    szam0 = szam1
+    szam1 = szam2
+    szam2 = szam0
 
 x = 0
-y = -1
-psz = szam1
-hatar = int(len(psz)/2)
-while x <= hatar:
-    mérő = False
-    if psz[x] == psz[y]:
-        x += 1
-        y -= 1
+while x < len(szam2):
+    if szam1[x] in '1234567890' and szam2[x] in '0123456789':
         mérő = True
     else:
-        break
+        mérő = False
+    x += 1
 
-print(mérő)
+
+if mérő == True:
+
+    if szam1 == '':
+        szam1 = 10
+    elif szam2 == '':
+        szam2 = 10
+
+    szam1 = int(szam1)
+    szam2 = int(szam2)
+
+    psz = szam1
+    hatar = int(len(str(psz))/2)
+
+    while psz <= szam2:
+        x = 0
+        y = -1
+        while x <= hatar:
+            mérő = False
+            if str(psz)[x] == str(psz)[y]:
+                x += 1
+                y -= 1
+                mérő = True
+            else:
+                break
+        if mérő == True:
+            print(psz)
+        psz +=1
+else:
+    print('Ez nem egy szám')
+
+
