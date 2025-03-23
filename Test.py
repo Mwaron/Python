c1b4459380e43294c713d308dda0429411b7c913
Mwaron <135520681+Mwaron@users.noreply.github.com>
2025-03-23 17:54:25 +0100

    sudoku_erettsegi

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print(f"{4}s")
+def resztabla(num):
+    if num <= 3:
+        return 1
+    elif num <= 6:
+        return 2
+    else:
+        return 3
+
+
+s = resztabla(3)
+o = resztabla(5)
+
+print((s-1)*3+o)
